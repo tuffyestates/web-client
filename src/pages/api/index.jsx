281e86fccedf63c6c9433f87a616fff56dcf037d
Ayrton Sparling <ayrton@sparling.us>
2018-10-24 22:15:49 -0700

    Fix API try-it-out resetting input fields on execute

--- src/pages/api/index.jsx
+++ src/pages/api/index.jsx
@@ -1,20 +1,53 @@
 import React from 'react';
+/** @jsx jsx */
+import {jsx} from '@emotion/core';
 
-export default class API extends React.PureComponent {
-    async componentDidMount() {
+// NOTE: As of swagger-ui 3.19.4, react 16 is not supported resulting in multiple
+// issues, namely inputs being reset before submitting a form. See
+// https://github.com/swagger-api/swagger-ui/labels/epic%3A%20usage%20in%20react%4016
+// for more info.
+export default class API extends React.Component {
+    constructor(props) {
+        super(props);
+        this.frame = React.createRef();
+    }
+    componentDidMount() {
         console.debug("Loading api schema from:", process.env.API_PATH);
+        const page = `
+<html>
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.19.4/swagger-ui-bundle.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.19.4/swagger-ui-standalone-preset.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/3.19.4/swagger-ui.css">
+</head>
+<body>
+    <div id="ui"></div>
+    <script>
+        window.onload = function() {
+            const ui = SwaggerUIBundle({
+                url: "${process.env.API_PATH}",
+                dom_id: '#ui',
+                presets: [
+                    SwaggerUIBundle.presets.apis,
+                    SwaggerUIStandalonePreset
+                ],
+            });
+            window.ui = ui;
+        }
+    </script>
+</body>
+</html>`;
+        const frame = this.frame.current;
 
-        // Load API theme
-        import (/* webpackChunkName: "api" */
-        './theme/main.scss');
-
-        // Load API ui
-        const SwaggerUI = (await import (/* webpackChunkName: "api" */
-        'swagger-ui')).default;
-
-        SwaggerUI({dom_id: "#ui", url: process.env.API_PATH});
+        const ifrmDoc = frame.contentWindow.document;
+        ifrmDoc.open();
+        ifrmDoc.write(page);
+        ifrmDoc.close();
+    }
+    shouldComponentUpdate() {
+        return false;
     }
     render() {
-        return (<div id="ui">Loading...</div>);
+        return (<iframe css={{width: '100%', height: '100%', border: 'none', display: 'block'}} ref={this.frame}/>);
     }
 }
