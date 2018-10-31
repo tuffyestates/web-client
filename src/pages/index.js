59b6ab5cc54425b75b6f740d7badc7df25ffb228
Ayrton Sparling <ayrton@sparling.us>
2018-10-30 22:59:14 -0700

    Prefetch the other pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import {lazy} from "react";
 export {default as Home} from "./home";
-export const Properties = lazy(() => import(/* webpackChunkName: "rest" */"./properties"));
-export const Register = lazy(() => import(/* webpackChunkName: "rest" */"./register"));
-export const Login = lazy(() => import(/* webpackChunkName: "rest" */"./login"));
-export const Property = lazy(() => import(/* webpackChunkName: "rest" */"./property"));
-export const FourOFour = lazy(() => import(/* webpackChunkName: "rest" */"./404"));
+export const Properties = lazy(() => import(/* webpackChunkName: "rest", webpackPrefetch: true */"./properties"));
+export const Register = lazy(() => import(/* webpackChunkName: "rest", webpackPrefetch: true */"./register"));
+export const Login = lazy(() => import(/* webpackChunkName: "rest", webpackPrefetch: true */"./login"));
+export const Property = lazy(() => import(/* webpackChunkName: "rest", webpackPrefetch: true */"./property"));
+export const FourOFour = lazy(() => import(/* webpackChunkName: "rest", webpackPrefetch: true */"./404"));
 
 export const API = lazy(() => import(/* webpackChunkName: "dev" */"./api"));
 export const Docs = lazy(() => import(/* webpackChunkName: "dev" */"./docs"));
