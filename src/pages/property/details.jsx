9ef7d9390452d6da9ec71e1512be262c5dabedc8
Unknown <MikeyK@csu.fullerton.edu>
2018-12-07 16:46:30 -0800

    Added optional code
    Threw in my commented out code if we wanted to change the Map to run with Google Maps in the future

--- src/pages/property/details.jsx
+++ src/pages/property/details.jsx
@@ -71,6 +71,55 @@ export class Map extends React.PureComponent {
     }
 }
 
+/*                Alternative Google Map Route Code
+
+                **Requires google API key and files**
+
+export class Container extends React.Component {
+  render() {
+    const style = {
+      width: '100vw',
+      height: '100vh'
+    }
+    return (
+      <div style={style}>
+        <Map google={this.props.google}
+          />
+      </div>
+    )
+  }
+}
+
+export class Map extends React.PureComponent {
+  componentDidMount() {
+    this.loadMap();
+  }
+  if (this.props && this.props.google) {
+      // google is available
+      const {google} = this.props;
+      const maps = google.maps;
+
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);
+
+      let zoom = 14;
+      let lat = 37.774929;
+      let lng = -122.419416;
+      const center = new maps.LatLng(lat, lng);
+      const mapConfig = Object.assign({}, {
+        center: center,
+        zoom: zoom
+      })
+      this.map = new maps.Map(node, mapConfig);
+    }
+
+  render() {
+
+  }
+}
+
+*/
+
 export class Features extends React.PureComponent {
     state = {};
     static getDerivedStateFromProps(props, state) {
